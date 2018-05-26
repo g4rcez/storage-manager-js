3fac29d16943ca608c6c3d6426108b8daa2e0797
Garcez <allan.f.garcez@gmail.com>
2018-05-26 15:17:51 -0300

    Prepare to publish

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-
-const isObject = (value) => {
+const isObject = value => {
 	return value && typeof value === 'object' && value.constructor === Object;
 };
 
-const isString = (value) => {
+const isString = value => {
 	return typeof value === 'string' || value instanceof String;
 };
 
 const objectForIn = (object, callback) => {
 	try {
-		Object.keys(object).map((item) => {
+		Object.keys(object).map(item => {
 			return callback(item);
 		});
-	} catch (error) { }
+	} catch (error) {}
 };
 
-const parseDateOrInteger = (string) => {
+const parseDateOrInteger = string => {
 	let date: any = new Date();
 	const withInteger: Date = new Date(date * 1 + string * 864e5);
 	return !!parseInt(string) ? withInteger : string;
@@ -25,7 +24,7 @@ const objectContains = (object, value) => {
 	if (!isObject(object)) return false;
 	return (
 		Object.keys(object)
-			.filter((item) => item === value)
+			.filter(item => item === value)
 			.toString() === value && isString(value)
 	);
 };
@@ -37,13 +36,13 @@ const storageOperator = (type, key = '', value = '') => {
 		parser: () => {
 			return window[type];
 		},
-		get: (key) => {
+		get: key => {
 			return window[type][key];
 		},
 		set: (key, value) => {
-			window[type][key] = value
+			window[type][key] = value;
 		},
-		unset: (key) => {
+		unset: key => {
 			window[type].removeItem(key);
 		},
 	};
@@ -54,16 +53,16 @@ const operator = {
 		parser: () => {
 			return storageOperator('localStorage').parser();
 		},
-		get: (key) => {
+		get: key => {
 			return storageOperator('localStorage').get(key);
 		},
 		set: (key, value) => {
 			storageOperator('localStorage').set(key, value);
 		},
-		unset: (key) => {
+		unset: key => {
 			try {
 				storageOperator('localStorage').unset(key);
-			} catch (error) { }
+			} catch (error) {}
 		},
 		clear: () => {
 			objectForIn(window.localStorage, operator.localstorage.unset);
@@ -73,16 +72,16 @@ const operator = {
 		parser: () => {
 			return storageOperator('sessionStorage').parser();
 		},
-		get: (key) => {
+		get: key => {
 			return storageOperator('sessionStorage').get(key);
 		},
 		set: (key, value) => {
 			storageOperator('sessionStorage').set(key, value);
 		},
-		unset: (key) => {
+		unset: key => {
 			try {
 				storageOperator('sessionStorage').unset(key);
-			} catch (error) { }
+			} catch (error) {}
 		},
 		clear: () => {
 			objectForIn(window.sessionStorage, operator.sessionstorage.unset);
@@ -92,21 +91,21 @@ const operator = {
 		parser: () => {
 			const cookies = document.cookie ? document.cookie.split('; ') : [];
 			if (cookies.length === 0) return;
-			return cookies.map((value) => value.split('=')).reduce((cookieAccumulator, cookieValue) => {
+			return cookies.map(value => value.split('=')).reduce((cookieAccumulator, cookieValue) => {
 				cookieAccumulator[decodeURIComponent(cookieValue[0])] = decodeURIComponent(cookieValue[1]);
 				return cookieAccumulator;
 			}, {});
 		},
-		set: (key, value, parameters = { path: '', domain: '', expires: '' }) => {
-			let path = parameters.path || '';
-			let domain = parameters.domain || '';
-			let expires = parseDateOrInteger(parameters.expires) || '';
-			document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}; expires=${expires}; path='${path}';domain='${domain}'`;
+		set: (key, val, parameters = { expires: '' }) => {
+			let exp = parseDateOrInteger(parameters.expires) || '';
+			!!exp
+				? (document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(val)};expires="${exp}";`)
+				: (document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(val)}`);
 		},
 		get: (key, expect) => {
 			return cache[key];
 		},
-		unset: (key) => {
+		unset: key => {
 			document.cookie = encodeURIComponent(key) + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 			cache[key] = undefined;
 		},
@@ -129,8 +128,8 @@ export default function StorageManage(manager) {
 	if (!!Storage) {
 		manager = managers[manager.toLowerCase()] || 'cookie';
 	} else {
-		console.warn("Browser doesn't have support to Storage")
-		manager = 'cookie'
+		console.warn("Browser doesn't have support to Storage");
+		manager = 'cookie';
 	}
 
 	return Object.freeze({
@@ -160,7 +159,7 @@ export default function StorageManage(manager) {
 	function json() {
 		let parser = operator[manager].parser(),
 			json = {};
-		Object.keys(parser).map((item) => {
+		Object.keys(parser).map(item => {
 			try {
 				json = { ...json, [item]: JSON.parse(parser[item]) };
 			} catch (error) {
@@ -184,7 +183,9 @@ export default function StorageManage(manager) {
 		try {
 			return expect === 'raw' || expect === 'r'
 				? value
-				: expect === 'array' || expect === 'a' ? value.split(',') : JSON.parse(value);
+				: expect === 'array' || expect === 'a'
+					? value.split(',')
+					: JSON.parse(value);
 		} catch (error) {
 			return value;
 		}
@@ -192,7 +193,7 @@ export default function StorageManage(manager) {
 
 	function set(key, value, expires = '') {
 		operator[manager].set(key, JSON.stringify(value), expires);
-		cache = { ...cache, [key]: value }
+		cache = { ...cache, [key]: value };
 	}
 
 	function unset(key) {
@@ -204,6 +205,6 @@ export default function StorageManage(manager) {
 	}
 
 	function clearAll() {
-		['cookie', 'localstorage', 'sessionstorage'].forEach(x => operator[x].clear())
+		['cookie', 'localstorage', 'sessionstorage'].forEach(x => operator[x].clear());
 	}
 }
