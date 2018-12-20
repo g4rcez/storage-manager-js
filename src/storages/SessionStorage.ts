aaad7f91d9d6a2c5f70872e51eaa8b724ab87e2e
Garcez <allan.f.garcez@gmail.com>
2018-12-20 02:18:28 -0200

    SessionStorage manipulate class

---
+++ src/storages/SessionStorage.ts
@@ -0,0 +1,33 @@
+import { IStorage, IParameters } from "../types";
+import { map } from "../utils";
+
+export default class SessionStorage implements IStorage {
+    parser() {
+        return window.sessionStorage;
+    }
+    clear(): IStorage {
+        map(window.sessionStorage, this.unset);
+        return this;
+    }
+    get(key: string) {
+        const string = window.sessionStorage.getItem(key);
+        try {
+            // @ts-ignore
+            return JSON.parse(string);
+        } catch (error) {
+            return string;
+        }
+    }
+    unset(key: string): IStorage {
+        try {
+            window.sessionStorage.removeItem(key);
+        } catch (error) {
+            window.sessionStorage.removeItem("");
+        }
+        return this;
+    }
+    set(key: string, object: any, parameters?: IParameters): IStorage {
+        window.sessionStorage.setItem(key, JSON.stringify(object));
+        return this;
+    }
+}
