9993315b8ce4b6a5bee849589d2bf94340a72d63
Garcez <allan.f.garcez@gmail.com>
2018-12-20 02:19:33 -0200

    New build for index

--- build/index.js
+++ build/index.js
@@ -1,1 +1,57 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var isStr=function(e){return"string"==typeof e||e instanceof String},fnDate=function(e){var t=new Date,o=new Date(1*t+864e5*e);return parseInt(e,10)?o:e},mapper=function(e,t){try{Object.keys(e).forEach(function(e){return t(e)})}catch(e){window.console.log(e)}};exports.Storages={cookie:"cookie",localStorage:"localstorage",sessionStorage:"sessionstorage"};var setExpires=function(e){return e.replace(/^ +/,"").replace(/=.*/,"=;expires="+(new Date).toUTCString()+";path=/")},op={cookie:{clear:function(){document.cookie.split(";").forEach(function(e){document.cookie=setExpires(e)})},get:function(e){return op.cookie.parser()[e]},parser:function(){return""===document.cookie?{}:document.cookie.split("; ").map(function(e){return e.split("=")}).reduce(function(e,t){return e[decodeURIComponent(t[0].trim())]=decodeURIComponent(t[1].trim()),e},{})},set:function(e,t,o){void 0===o&&(o={expires:"1969-12-31T23:59:59.000Z",path:"/"}),document.cookie=encodeURIComponent(e)+"="+decodeURIComponent(t)+";path="+o.path+";expires="+fnDate(o.expires)},unset:function(e){document.cookie=encodeURIComponent(e)+"=;"+(new Date).toUTCString()}},localstorage:{clear:function(){mapper(window.localStorage,op.localstorage.unset)},get:function(e){return window.localStorage.getItem(e)},parser:function(){return window.localStorage},set:function(e,t){return window.localStorage.setItem(e,t)},unset:function(e){try{window.localStorage.removeItem(e)}catch(e){window.localStorage.removeItem("")}}},sessionstorage:{clear:function(){mapper(window.sessionStorage,op.sessionstorage.unset)},get:function(e){return window.sessionStorage.getItem(e)},parser:function(){return window.sessionStorage},set:function(e,t){return window.sessionStorage.setItem(e,t)},unset:function(e){try{window.sessionStorage.removeItem(e)}catch(e){window.sessionStorage.removeItem("")}}}},normalize=function(e){return e.toLowerCase().trim()},getManager=function(e){var t=normalize(e);return 0<=["cookie","localstorage","sessionstorage"].indexOf(t)?t:normalize(exports.Storages.cookie)};function StorageManagerJs(n){return void 0===n&&(n="cookie"),n=Storage?getManager(n):"cookie",{all:e,cat:t,change:function(e){void 0===e&&(e="cookie");return n=getManager(e),this},clear:function(){return op[n].clear(),this},clearAll:i,create:o,delete:r,get:t,getItem:t,item:t,json:e,purge:i,remove:r,rm:r,set:o,setItem:o,touch:o,unset:r};function e(){return op[n].parser()}function t(e,t){var o=op[n].get(e);try{return"raw"===t||"r"===t?o:"array"===t||"a"===t?o.split(","):JSON.parse(o)}catch(e){return o}}function o(e,t,o){return isStr(t)?op[n].set(e,t.trim(),o):op[n].set(e,JSON.stringify(t),o),this}function r(e){return op[n].unset(e),this}function i(){return["cookie","localstorage","sessionstorage"].forEach(function(e){return op[e].clear()}),this}}exports.default=StorageManagerJs;
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var types_1 = require("./types");
+var Cookie_1 = require("./storages/Cookie");
+var LocalStorage_1 = require("./storages/LocalStorage");
+var SessionStorage_1 = require("./storages/SessionStorage");
+var Cookie_2 = require("./storages/Cookie");
+exports.Cookie = Cookie_2.default;
+var LocalStorage_2 = require("./storages/LocalStorage");
+exports.LocalStorage = LocalStorage_2.default;
+var SessionStorage_2 = require("./storages/SessionStorage");
+exports.SessionStorage = SessionStorage_2.default;
+exports.Storages = __assign({}, types_1.Storages);
+var normalize = function (str) { return str.toLowerCase().trim(); };
+var managers = {
+    cookie: new Cookie_1.default(),
+    localstorage: new LocalStorage_1.default(),
+    sessionstorage: new SessionStorage_1.default(),
+};
+var getManager = function (value) {
+    var manager = normalize(value);
+    return managers[manager];
+};
+function StorageManagerJs(managerName) {
+    if (managerName === void 0) { managerName = "cookie"; }
+    var manager = getManager(managerName);
+    return {
+        all: function () { return manager.parser(); },
+        json: function () { return manager.parser(); },
+        cat: function (key) { return manager.get(key); },
+        get: function (key) { return manager.get(key); },
+        item: function (key) { return manager.get(key); },
+        getItem: function (key) { return manager.get(key); },
+        clear: function (key) { return manager.unset(key); },
+        rm: function (key) { return manager.unset(key); },
+        unset: function (key) { return manager.unset(key); },
+        delete: function (key) { return manager.unset(key); },
+        remove: function (key) { return manager.unset(key); },
+        set: function (key, value, params) { return manager.set(key, value, params); },
+        touch: function (key, value, params) { return manager.set(key, value, params); },
+        create: function (key, value, params) { return manager.set(key, value, params); },
+        setItem: function (key, value, params) { return manager.set(key, value, params); },
+    };
+}
+exports.StorageManagerJs = StorageManagerJs;
+//# sourceMappingURL=index.js.map
