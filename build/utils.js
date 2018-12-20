7d5a18e18523139a7ed617005a934fcacf634d6c
Garcez <allan.f.garcez@gmail.com>
2018-12-20 02:20:38 -0200

    Utils functions

---
+++ build/utils.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isStr = function (value) { return typeof value === "string" || value instanceof String; };
+exports.fnDate = function (str) {
+    var date = new Date();
+    var integer = new Date(date * 1 + str * 864e5);
+    return !!parseInt(str, 10) ? integer : str;
+};
+exports.map = function (object, callback) {
+    try {
+        Object.keys(object).forEach(function (x) { return callback(x); });
+    }
+    catch (error) {
+        window.console.log(error);
+    }
+};
+exports.setExpires = function (cookie) {
+    return cookie.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+};
+//# sourceMappingURL=utils.js.map
