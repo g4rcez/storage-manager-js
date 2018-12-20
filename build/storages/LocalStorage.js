dc5d5a23a91289ec6993f365458eb182d1e86b4e
Garcez <allan.f.garcez@gmail.com>
2018-12-20 02:20:02 -0200

    LocalStorage build

---
+++ build/storages/LocalStorage.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var utils_1 = require("../utils");
+var LocalStorage = /** @class */ (function () {
+    function LocalStorage() {
+    }
+    LocalStorage.prototype.parser = function () {
+        return window.localStorage;
+    };
+    LocalStorage.prototype.clear = function () {
+        utils_1.map(window.localStorage, this.unset);
+        return this;
+    };
+    LocalStorage.prototype.get = function (key) {
+        var string = window.localStorage.getItem(key) || "";
+        try {
+            // @ts-ignore
+            return JSON.parse(string);
+        }
+        catch (error) {
+            return string;
+        }
+    };
+    LocalStorage.prototype.unset = function (key) {
+        try {
+            window.localStorage.removeItem(key);
+        }
+        catch (error) {
+            window.localStorage.removeItem("");
+        }
+        return this;
+    };
+    LocalStorage.prototype.set = function (key, object, parameters) {
+        window.localStorage.setItem(key, JSON.stringify(object));
+        return this;
+    };
+    return LocalStorage;
+}());
+exports.default = LocalStorage;
+//# sourceMappingURL=LocalStorage.js.map
