b2cf0dc0ce83502bf6b53e8695d974c759bc1d24
Garcez <allan.f.garcez@gmail.com>
2018-12-20 02:20:31 -0200

    Types

---
+++ build/types.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Storages = {
+    cookie: "cookie",
+    localStorage: "localstorage",
+    sessionStorage: "sessionstorage",
+};
+//# sourceMappingURL=types.js.map
