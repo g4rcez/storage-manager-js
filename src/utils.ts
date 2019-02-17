87256dfdd7ca41f8e38e7ec3a43580a603e972ff
Garcez <allan.f.garcez@gmail.com>
2019-02-16 23:39:25 -0200

    Format utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,18 @@
 export const isStr = (value: any) => typeof value === "string" || value instanceof String;
 
 export const fnDate = (str: any) => {
-    const date: any = new Date();
-    const integer = new Date(date * 1 + str * 864e5);
-    return !!parseInt(str, 10) ? integer : str;
+	const date: any = new Date();
+	const integer = new Date(date * 1 + str * 864e5);
+	return !!parseInt(str, 10) ? integer : str;
 };
 
 export const map = (object: object, callback: any): void => {
-    try {
-        Object.keys(object).forEach((x) => callback(x));
-    } catch (error) {
-        window.console.log(error);
-    }
+	try {
+		Object.keys(object).forEach((x) => callback(x));
+	} catch (error) {
+		window.console.log(error);
+	}
 };
 
 export const setExpires = (cookie: string) =>
-    cookie.replace(/^ +/, "").replace(/=.*/, `=;expires=${new Date().toUTCString()};path=/`);
+	cookie.replace(/^ +/, "").replace(/=.*/, `=;expires=${new Date().toUTCString()};path=/`);
