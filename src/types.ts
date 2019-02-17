1c8ad191abfa50467514dbe2733d9b98e20359c8
Garcez <allan.f.garcez@gmail.com>
2019-02-16 23:39:13 -0200

    format types

--- src/types.ts
+++ src/types.ts
@@ -1,36 +1,36 @@
 export type IParameters = {
-    path?: string;
-    expires?: number | string;
+	expires?: number | string;
+	path?: string;
 };
 
 export type IStorageManager = {
-    remove(key: string): IStorage;
-    delete(key: string): IStorage;
-    setItem(key: string, value: any, params?: IParameters): IStorage;
-    getItem(key: string): any;
-    create(key: string, value: any, params?: IParameters): IStorage;
-    touch(key: string, value: any, params?: IParameters): IStorage;
-    rm(key: string): IStorage;
-    item(key: string): any;
-    all(): any;
-    cat(key: string): any;
-    unset(key: string): IStorage;
-    get(key: string): any;
-    set(key: string, value: any, params?: IParameters): IStorage;
-    json(): any;
-    clear(key: string): IStorage;
+	all(): any;
+	cat(key: string): any;
+	clear(key: string): IStorage;
+	create(key: string, value: any, params?: IParameters): IStorage;
+	delete(key: string): IStorage;
+	get(key: string): any;
+	getItem(key: string): any;
+	item(key: string): any;
+	json(): string;
+	remove(key: string): IStorage;
+	rm(key: string): IStorage;
+	set(key: string, value: any, params?: IParameters): IStorage;
+	setItem(key: string, value: any, params?: IParameters): IStorage;
+	touch(key: string, value: any, params?: IParameters): IStorage;
+	unset(key: string): IStorage;
 };
 
 export const Storages = {
-    cookie: "cookie",
-    localStorage: "localstorage",
-    sessionStorage: "sessionstorage",
+	cookie: "cookie",
+	localStorage: "localstorage",
+	sessionStorage: "sessionstorage",
 };
 
 export interface IStorage {
-    parser(): any;
-    clear(): IStorage;
-    get(key: string): any;
-    unset(key: string): IStorage;
-    set(key: string, object: any, parameters?: IParameters): IStorage;
+	clear(): IStorage;
+	get(key: string): any;
+	parser(): any;
+	set(key: string, object: any, parameters?: IParameters): IStorage;
+	unset(key: string): IStorage;
 }
