925ac0645b5a3760c42c0f2f860949ae4d2dc5a3
Garcez <allan.f.garcez@gmail.com>
2019-02-16 23:39:04 -0200

    Fix bugs in storage get

--- src/storages/LocalStorage.ts
+++ src/storages/LocalStorage.ts
@@ -1,32 +1,31 @@
-import { IStorage, IParameters } from "./../types";
+import { IStorage } from "./../types";
 import { map } from "../utils";
 export default class LocalStorage implements IStorage {
-    parser() {
-        return window.localStorage;
-    }
-    clear(): IStorage {
-        map(window.localStorage, this.unset);
-        return this;
-    }
-    get(key: string) {
-        const string: string = window.localStorage.getItem(key) || "";
-        try {
-            // @ts-ignore
-            return JSON.parse(string);
-        } catch (error) {
-            return string;
-        }
-    }
-    unset(key: string): IStorage {
-        try {
-            window.localStorage.removeItem(key);
-        } catch (error) {
-            window.localStorage.removeItem("");
-        }
-        return this;
-    }
-    set(key: string, object: any, parameters?: IParameters): IStorage {
-        window.localStorage.setItem(key, JSON.stringify(object));
-        return this;
-    }
+	parser() {
+		return window.localStorage;
+	}
+	clear(): IStorage {
+		map(window.localStorage, this.unset);
+		return this;
+	}
+	get(key: string) {
+		const string: string = window.localStorage.getItem(key) || "";
+		try {
+			return JSON.parse(string);
+		} catch (error) {
+			return string;
+		}
+	}
+	unset(key: string): IStorage {
+		try {
+			window.localStorage.removeItem(key);
+		} catch (error) {
+			window.localStorage.removeItem("");
+		}
+		return this;
+	}
+	set(key: string, object: any): IStorage {
+		window.localStorage.setItem(key, JSON.stringify(object));
+		return this;
+	}
 }
--- src/storages/SessionStorage.ts
+++ src/storages/SessionStorage.ts
@@ -1,33 +1,32 @@
-import { IStorage, IParameters } from "../types";
+import { IStorage } from "../types";
 import { map } from "../utils";
 
 export default class SessionStorage implements IStorage {
-    parser() {
-        return window.sessionStorage;
-    }
-    clear(): IStorage {
-        map(window.sessionStorage, this.unset);
-        return this;
-    }
-    get(key: string) {
-        const string = window.sessionStorage.getItem(key);
-        try {
-            // @ts-ignore
-            return JSON.parse(string);
-        } catch (error) {
-            return string;
-        }
-    }
-    unset(key: string): IStorage {
-        try {
-            window.sessionStorage.removeItem(key);
-        } catch (error) {
-            window.sessionStorage.removeItem("");
-        }
-        return this;
-    }
-    set(key: string, object: any, parameters?: IParameters): IStorage {
-        window.sessionStorage.setItem(key, JSON.stringify(object));
-        return this;
-    }
+	parser() {
+		return window.sessionStorage;
+	}
+	clear(): IStorage {
+		map(window.sessionStorage, this.unset);
+		return this;
+	}
+	get(key: string) {
+		const string = window.sessionStorage.getItem(key) || "";
+		try {
+			return JSON.parse(string);
+		} catch (error) {
+			return string;
+		}
+	}
+	unset(key: string): IStorage {
+		try {
+			window.sessionStorage.removeItem(key);
+		} catch (error) {
+			window.sessionStorage.removeItem("");
+		}
+		return this;
+	}
+	set(key: string, object: any): IStorage {
+		window.sessionStorage.setItem(key, JSON.stringify(object));
+		return this;
+	}
 }
