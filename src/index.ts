d1e7c1de48ab5599920dfa6853ead5c428bc0b3b
Garcez <allan.f.garcez@gmail.com>
2019-11-04 11:11:08 -0300

    fix with lint and change Storages to enum

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,47 @@
-import { IStorage, IStorageManager, Storages as StorageTypes, IParameters } from "./types";
 import Cookie from "./storages/Cookie";
 import LocalStorage from "./storages/LocalStorage";
 import SessionStorage from "./storages/SessionStorage";
+import { IParameters, IStorage, IStorageManager, Storages as StorageTypes } from "./types";
 export { default as Cookie } from "./storages/Cookie";
 export { default as LocalStorage } from "./storages/LocalStorage";
 export { default as SessionStorage } from "./storages/SessionStorage";
-export const Storages = { ...StorageTypes };
+export const Storages = StorageTypes;
 
-type Managers = {
-	cookie(): IStorage;
-	localstorage(): IStorage;
-	sessionstorage(): IStorage;
-};
-
-function normalize(str: string) {
-	return str.toLowerCase().trim();
+interface IManagers {
+	[Storages.Cookie]: () => IStorage;
+	[Storages.LocalStorage]: () => IStorage;
+	[Storages.SessionStorage](): IStorage;
 }
-const managers: Managers = {
+
+const normalize = (str: string) => str.toLowerCase().trim();
+
+const managers: IManagers = {
 	cookie: () => new Cookie(),
 	localstorage: () => new LocalStorage(),
 	sessionstorage: () => new SessionStorage(),
 };
 
-function getManager(value: string): IStorage {
-	return managers[normalize(value)];
-}
+const getManager = (value: string): IStorage => managers[normalize(value)];
 
-export function StorageManagerJs(managerName: string = "cookie"): IStorageManager {
+type managersAccept = "cookie" | "localStorage" | "sessionStorage";
+
+export function StorageManagerJs(managerName: managersAccept = "cookie"): IStorageManager {
 	const manager = getManager(managerName);
 	return Object.freeze({
-		all: () => manager.parser(),
-		cat: (key: string) => manager.get(key),
-		clear: (key: string) => manager.unset(key),
+		all: manager.parser,
+		cat: manager.get,
+		clear: manager.unset,
 		create: (key: string, value: any, params?: IParameters) => manager.set(key, value, params),
-		delete: (key: string) => manager.unset(key),
-		get: (key: string) => manager.get(key),
-		getItem: (key: string) => manager.get(key),
-		item: (key: string) => manager.get(key),
+		delete: manager.unset,
+		get: manager.get,
+		getItem: manager.get,
+		item: manager.get,
 		json: () => JSON.stringify(manager.parser()),
-		remove: (key: string) => manager.unset(key),
-		rm: (key: string) => manager.unset(key),
+		remove: manager.unset,
+		rm: manager.unset,
 		set: (key: string, value: any, params?: IParameters) => manager.set(key, value, params),
 		setItem: (key: string, value: any, params?: IParameters) => manager.set(key, value, params),
 		touch: (key: string, value: any, params?: IParameters) => manager.set(key, value, params),
-		unset: (key: string) => manager.unset(key),
+		unset: manager.unset,
 	});
 }
