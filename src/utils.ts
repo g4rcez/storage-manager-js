be7ddd0e214b08e98d73820d978cd337f866070e
g4rcez <allan.f.garcez@gmail.com>
2020-02-16 02:17:52 -0300

    remove unused code

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,3 @@
-export const isStr = (value: any) => typeof value === "string" || value instanceof String;
-
-export const fnDate = (str: number | string) => {
-	const date: any = new Date();
-	return typeof str === "number" ? new Date(date * 1 + (str as number) * 864e5) : str;
-};
-
 export const map = (object: object, callback: any): void => {
 	try {
 		Object.keys(object).forEach((x) => callback(x));
