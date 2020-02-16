39cc08b9253defb90c30e15af27724fa1d59ef97
g4rcez <allan.f.garcez@gmail.com>
2020-02-16 02:16:09 -0300

    bili build project

---
+++ .bilirc.ts
@@ -0,0 +1,20 @@
+import { Config as Configuration } from "bili";
+
+const configuration: Configuration = {
+	banner: false,
+	input: "src/index.ts",
+	output: {
+		format: ["es", "cjs", "umd", "umd-min"],
+		moduleName: "StorageManager",
+		dir: "build",
+	},
+	plugins: {
+		typescript2: {
+			clean: true,
+			tsconfig: "./tsconfig.build.json",
+			useTsconfigDeclarationDir: true,
+		},
+	},
+};
+
+export default configuration;
